8190985b0cd27a5fa31ba4ce951a55e52eaf535b
31338283 <31338283@087998-P33S105.macklabs.br>
2016-02-23 08:57:55 -0300

    Notacoes para BD

--- LP3Novo-war/src/java/com/br/lp3/entities/UserLP3.java
+++ LP3Novo-war/src/java/com/br/lp3/entities/UserLP3.java
@@ -2,18 +2,47 @@ package com.br.lp3.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 
 /**
  *
  * @author 31338283
  */
+@Entity
+@Table(name = "UserLP3")
 public class UserLP3 implements Serializable {
 
+    @Id
+    @Column(name = "id_user")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id_user;
+    
+    @Column(name = "username")
+    @Size(max = 10)
     private String username;
+    
+    @Column(name = "password")
+    @Size(max = 32)
     private String password;
+    
+    @Column(name = "fullname")
+    @Size(max = 100)
     private String fullname;
+    
+    @Column(name = "email")
+    @Size(max = 150)
     private String email;
+    
+    @Column(name = "birthday")
+    @Temporal(TemporalType.DATE)
     private Date birthday;
 
     public UserLP3() {
